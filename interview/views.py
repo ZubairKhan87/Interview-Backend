10b0f3655b90ab03f95a0d333b3b8d44119ca7b2
bairi56 <zubair413427@gmail.com>
2025-05-13 00:05:07 +0500

    frontend integration

--- interview/views.py
+++ interview/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
-from huggingface_hub import InferenceClient
 from django.views.decorators.csrf import csrf_exempt
 import re
 import csv
