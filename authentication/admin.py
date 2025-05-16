d8e0bcbf33e12a268324f6aae83292c1ad04e2e0
bairi56 <zubair413427@gmail.com>
2025-05-16 13:06:29 +0500

    updated in profile

--- authentication/admin.py
+++ authentication/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 from .models import UserProfile,CandidateTable,RecruiterTable
 # # Register your models here.
 admin.site.register(UserProfile)
-admin.site.register(CandidateTable)
+class CandidateTableAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        # Pass the current user to  model's save method
+        obj.save(request_user=request.user)
+
+admin.site.register(CandidateTable, CandidateTableAdmin)
 admin.site.register(RecruiterTable)
